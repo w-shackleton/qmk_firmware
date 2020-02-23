ee1b5bbed7a2f1ec52111e08d6f350d5b4e3f2f2
Will Shackleton <w.shackleton@gmail.com>
2020-02-23 17:32:27 +0000

    Update keyboard for work usage with OSX

--- keyboards/keebio/iris/keymaps/w-shackleton/keymap.c
+++ keyboards/keebio/iris/keymaps/w-shackleton/keymap.c
@@ -6,6 +6,13 @@
 #define _RAISE 2
 #define _ADJUST 3
 
+// Apple backtick char
+#define AP_BTICK KC_BSLS
+// Apple backslash char
+#define AP_BSLS KC_GRAVE
+
+#define CS_CTRL MT(MOD_LCTL, AP_BSLS)
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -17,15 +24,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+     AP_BTICK,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL, 
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC,          KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NUHS,
+     CS_CTRL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC,          KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NUHS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    RAISE,   KC_LALT, KC_ENT,                    KC_SPC,  KC_ESC,  LOWER 
+                                    RAISE,   KC_LCMD, KC_ENT,                    KC_SPC,  KC_ESC,  LOWER 
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
